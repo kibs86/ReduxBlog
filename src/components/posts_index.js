91cbb2d366e804e0bf0f32fe4e3cc11aa4109920
kibs86 <kibs086@gmail.com>
2017-02-01 16:01:56 -0500

    Promote posts_index to a container
    - create dummy componentWillMount() function

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return <div>List of blog posts.</div>;
+class PostsIndex extends Component {
+  // lifecycle method - if a function is named like below, React will call this automatically whenever component
+  // is about to be rendered to the DOM for the first time.  It won't be called on re-renders.
+  componentWillMount() {
+    //console.log('this would be a good time to call an action creator to fetch posts');
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div>
+    );
+  }
 }
+
+export default PostsIndex;
