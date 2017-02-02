7a11f21771db83a9e19f70f2227456418908e452
kibs86 <kibs086@gmail.com>
2017-02-02 09:00:32 -0500

    Display list of posts on index page
    - Create mapStateToProps and export it
    - Within render method, display a list of posts using
      renderPosts helper method
    - Create renderPosts helper method to return an <li> for each
      post

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -11,6 +11,17 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
@@ -19,11 +30,18 @@ class PostsIndex extends Component {
             Add a Post
           </Link>
         </div>
-        List of blog posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
+}
+
 // below is a shortcut for mapDispatchToProps and fetchPosts is the same as fetchPosts: fetchPosts
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
