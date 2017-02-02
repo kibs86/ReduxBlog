c410e8edf2ffdf4a2ca0ceb6aecde7e200dc7c7d
kibs86 <kibs086@gmail.com>
2017-02-01 20:37:17 -0500

    create button on index page to link to /posts/new

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
   // lifecycle method - if a function is named like below, React will call this automatically whenever component
@@ -12,7 +13,14 @@ class PostsIndex extends Component {
 
   render() {
     return (
-      <div>List of blog posts</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        List of blog posts
+      </div>
     );
   }
 }
